85cc0bb589ccad45fcbbf65168b9cfb3c720714c
danny <dannytakushi@gmail.com>
2025-06-09 01:09:32 -0500

    index iteration; 2nd iteration limits to i+1

--- leetcode/1_two_sum/two_sum.rb
+++ leetcode/1_two_sum/two_sum.rb
@@ -5,15 +5,16 @@ require 'debug'
 def two_sum(nums, target)
   # binding.break
 
-  nums.each_with_index do |n1, idx1|
-    nums.each_with_index do |n2, idx2|
-      if n1 + n2 == target
-        return [idx1,idx2]
+  # nums.each_with_index do |n1, idx1|
+  for i in 0...nums.length
+    # nums.each_with_index do |n2, idx2|
+    for j in (i + 1)...nums.length
+      if (nums[i] + nums[j] == target)
+        return [i,j]
       end
     end
   end
 end
 
 
-
-# two_sum([2, 7, 11, 15], 9)
+# two_sum([3, 2, 4], 6)
