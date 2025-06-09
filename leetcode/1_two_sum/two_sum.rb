e346d5af6263fec0f81e9d5a21561c48dfc91f2d
danny <dannytakushi@gmail.com>
2025-06-08 23:37:44 -0500

    unit-test content added to file

--- leetcode/1_two_sum/two_sum.rb
+++ leetcode/1_two_sum/two_sum.rb
@@ -1,7 +1,21 @@
+require 'test/unit'
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-    
+    return [0,1]
 end
 
+class MyTest <Test::Unit::TestCase
+  def test_two_sum_ex1
+    assert_equal [0, 1], two_sum([2, 7, 11, 15], 9)
+  end
+
+  def test_two_sum_ex2
+    assert_equal [1, 2], two_sum([3, 2, 4], 6)
+  end
+
+  def test_two_sum_ex3
+    assert_equal [0, 1], two_sum([3, 3], 6)
+  end
+end
