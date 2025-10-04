3f1ec482ca676a3ebaad6d29b63263b02529b6b9
danny <dannytakushi@gmail.com>
2025-10-04 15:02:22 -0500

    random time; header rows

--- csv/csvplayer.rb
+++ csv/csvplayer.rb
@@ -7,8 +7,16 @@ def generate_code(number_of_chars)
   return output
 end
 
+def time_rand from = 0.0, to = Time.now
+  t = Time.at(from + rand * (to.to_f - from.to_f))
+  out = t.strftime('%m-%d-%Y')
+  return out
+end
+
 def gencsv
-  out = CSV.generate_line([generate_code(10), '04321'])
+  out = CSV.generate_line(["company name","date","description"])
+
+  out += CSV.generate_line([generate_code(10), time_rand()])
   out += CSV.generate_line(['bar', '100'])
   out += CSV.generate_line(['jiffy', 122])
   out += CSV.generate_line(['ipsum lorem', 'm1 garand'])
