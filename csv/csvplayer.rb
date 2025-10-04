a1f467594ea36c94fcdbf6ebfc6090d2c7f02a3d
danny <dannytakushi@gmail.com>
2025-10-04 14:53:35 -0500

    generate_code for string

--- csv/csvplayer.rb
+++ csv/csvplayer.rb
@@ -1,7 +1,14 @@
 require 'csv'
 
+def generate_code(number_of_chars)
+  #https://www.rubyguides.com/2015/03/ruby-random/
+  charset = Array('A'..'Z') + Array('a'..'z')
+  output = Array.new(number_of_chars) { charset.sample }.join
+  return output
+end
+
 def gencsv
-  out = CSV.generate_line(['foo', '04321'])
+  out = CSV.generate_line([generate_code(10), '04321'])
   out += CSV.generate_line(['bar', '100'])
   out += CSV.generate_line(['jiffy', 122])
   out += CSV.generate_line(['ipsum lorem', 'm1 garand'])
